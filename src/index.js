6e7fe7aea915b943f22746828dc5b6e3e416d88f
MikalaiKrasko <mikalai.krasko@gmail.com>
2020-10-13 17:30:11 +0300

    init: initial task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+    const numAsStr = String(Math.abs(n));
+    let reversedStr = '';
+    let i = numAsStr.length - 1;
   
-}
+    while (i >= 0) {
+      reversedStr += numAsStr[i];
+      i -= 1;
+    }
+    const reversedModule = Number(reversedStr);
+  
+    return reversedModule;
+  };
